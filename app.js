fd4ad48082eafc560ebf67cd5bedf07ee8ef41c7
Alegma <6178232+alegmal@users.noreply.github.com>
2025-01-24 00:07:18 +0200

    formatting

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
-const HttpError = require("./modules/http-error");
-const { error } = require("console");
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./modules/http-error');
+const { error } = require('console');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/places", placesRoutes);
-app.use("/api/users", usersRoutes);
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  HttpError("Could not find this route.", 404);
+  HttpError('Could not find this route.', 404);
   throw error;
 });
 
@@ -24,7 +25,14 @@ app.use((error, req, res, next) => {
   }
 
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occured" });
+  res.json({ message: error.message || 'An unknown error occured' });
 });
 
-app.listen(5000);
+mongoose
+  .connect()
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
